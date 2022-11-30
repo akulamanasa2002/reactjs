65f1ea119881246e0bc083f34aaa69f3c2b88e1f
Ramani-Narasimha <thunuguntlaramani@gmail.com>
2022-11-30 12:03:30 +0530

    reactprops

---
+++ src/App.js
@@ -0,0 +1,30 @@
+/*import ClassComponent2 from './ClassComponent2'
+function App(){
+  return(
+    <section>
+      <p>hello everyone</p>
+      <h1>This is h1 tag</h1>
+     <img src="https://i.pinimg.com/736x/10/35/81/103581ad3841dd4d45df056695313e9e.jpg"width="30%"alt=""/>
+     <ClassComponent2/>
+    </section>
+  
+    )
+}
+export default App*/
+
+
+
+
+import PropsExample from './PropsExample'
+function App(){
+  return(
+        <div>
+        <h1>This is about props example</h1>
+        <PropsExample name="manasa" age="20"/>
+        </div>
+    )
+}
+
+export default App
+
+
---
+++ src/ClassComponent2.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class ClassComponent2 extends React.Component{
+	render(){
+		return(
+			<div>
+               From class component     
+			</div>
+
+			)
+	}
+
+}
+export default ClassComponent2
