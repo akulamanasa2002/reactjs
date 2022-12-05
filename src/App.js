847ff098d5270fda49876ed7aaafff14f370ce90
Ramani-Narasimha <thunuguntlaramani@gmail.com>
2022-12-05 11:54:11 +0530

    components

--- src/App.js
+++ src/App.js
@@ -18,18 +18,35 @@ export default App*/
 //import PropsExample from './PropsExample'
 //import FuncPropExample from './FuncPropExample'
 //import StateExample from "./StateExample"
-import Events from './Events'
+//import Events from './Events'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './Components/Home'
+import About from './Components/About'
+import Contact from './Components/Contact'
+import Navbar from './Components/Navbar'
+import NotFound from './Components/NotFound'
 function App(){
   return(
-        <div>
+    <section>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+             <Route path="/" element={<Home/>}/> 
+             <Route path="/about" element={<About/>}/> 
+             <Route path="/contact" element={<Contact/>}/> 
+             <Route path="*" element={<NotFound/>}/>
+    </Routes>
+    </BrowserRouter>
+
+
+        
        {/* <h1>This is about props example</h1>
       <PropsExample name="manasa" age="19"/>
         <PropsExample name="lalitha" age="20"/>
         <PropsExample name="ramya" age="20"/>*/}
        {/* <FuncPropExample framework="react" number="3"/>
        <StateExample/>*/}
-       <Events/>
-        </div>
+      </section>
     )
 }
 
